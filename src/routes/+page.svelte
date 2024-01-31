a4734d83ff1755b2c0a7d1e8ddb9f5bcf162ae3e
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-01-30 23:14:53 -0800

    Buggy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,40 @@
 <script lang="ts">
 	const Bmap = new Map([
-		[0, '0000'],
-		[1, '1000'],
-		[2, '0100'],
-		[3, '1100'],
-		[4, '0010'],
-		[5, '1010'],
-		[6, '0110'],
-		[7, '1110'],
-		[8, '0001'],
-		[9, '1001'],
-		[10, '0101']
+		[0, ['', '', '', '']],
+		[1, ['active', '', '', '']],
+		[2, ['', 'active', '', '']],
+		[3, ['active', 'active', '', '']],
+		[4, ['', '', 'active', '']],
+		[5, ['active', 'active', '', '']],
+		[6, ['', 'active', 'active', '']],
+		[7, ['active', 'active', 'active', '']],
+		[8, ['', '', '', 'active']],
+		[9, ['active', '', '', 'active']],
+		[10, ['', 'active', '', 'active']]
 	])
 
-	let time = $state<string[]>('00:00:00')
-	let hours = $state<string[]>([])
-	let minutes = $state<string[]>([])
-	let seconds = $state<string[]>([])
+	let time = $state<string>('00:00:00')
+	let hours = $state<string[]>(['0', '0'])
+	let minutes = $state<string[] | string>(['0', '0'])
+	let seconds = $state<string[] | string>(['0', '0'])
 
-	function toBinary(arr: number[]): string[] {
+	let bHours = $state<string[]>([])
+	let bHours1 = $state<string[] | string>([])
+	let bHours2 = $state<string[] | string>([])
+
+	let bMinutes = $state<string[]>([])
+	let bMinutes1 = $state<string[] | string>([])
+	let bMinutes2 = $state<string[] | string>([])
+
+	let bSeconds = $state<string[]>([])
+	let bSeconds1 = $state<string[] | string>([])
+	let bSeconds2 = $state<string[] | string>([])
+
+	function toBinary(arr: string[]): string[] {
+		console.log('arr: ', arr)
 		const results: string[] = []
+
 		for (let i = 0; i < arr.length; i++) {
-			console.log(`${i}: `, Bmap.get(parseInt(arr[i])), Bmap.get(parseInt(arr[i])))
 			results.push(Bmap.get(parseInt(arr[i])))
 		}
 		return results
@@ -35,42 +48,115 @@
 		const [h, m, s] = t.split(':')
 
 		hours = h.split('')
+		if (hours.length === 1) {
+			hours.unshift('0')
+		}
 		minutes = m.split('')
 		const [s1, s2] = s.split(' ')
 		seconds = s1.split('')
-		console.log(toBinary(seconds))
+
+		bHours = toBinary(hours)
+		bHours1 = toBinary(Array.from(hours[0]))
+		bHours2 = toBinary(Array.from(hours[1]))
+		bHours1 = bHours1[0]
+		bHours2 = bHours2[0]
+
+		bMinutes = toBinary(minutes)
+		bMinutes1 = toBinary(Array.from(minutes[0]))
+		bMinutes2 = toBinary(Array.from(minutes[1]))
+		bMinutes1 = bMinutes1[0]
+		bMinutes2 = bMinutes2[0]
+
+		bSeconds = toBinary(seconds)
+		bSeconds1 = toBinary(Array.from(seconds[0]))
+		bSeconds2 = toBinary(Array.from(seconds[1]))
+		bSeconds1 = bSeconds1[0]
+		bSeconds2 = bSeconds2[0]
 	}, 1000)
 </script>
 
 <section>
 	<div class="flex">
-		<p>{hours[0]}</p>
-		<p>{hours[1]}</p>
-		:
-		<p>{minutes[0]}</p>
-		<p>{minutes[1]}</p>
-		:
-		<p>{seconds[0]}</p>
-		<p>{seconds[1]}</p>
+		<div class="stack">
+			<div class="digit">1</div>
+			<div class="digit">2</div>
+			<div class="digit">4</div>
+			<div class="digit">8</div>
+		</div>
+		<div class="stack">
+			{#each bHours1 as dotStatus}
+				<div class="dot {dotStatus}" />
+			{/each}
+		</div>
+		<div class="stack">
+			{#each bHours1 as dotStatus}
+				<div class="dot {dotStatus}" />
+			{/each}
+		</div>
+		<div class="stack">
+			{#each bMinutes1 as dotStatus}
+				<div class="dot {dotStatus}" />
+			{/each}
+		</div>
+		<div class="stack">
+			{#each bMinutes2 as dotStatus}
+				<div class="dot {dotStatus}" />
+			{/each}
+		</div>
+		<div class="stack">
+			{#each bSeconds1 as dotStatus}
+				<div class="dot {dotStatus}" />
+			{/each}
+		</div>
+		<div class="stack">
+			{#each bSeconds2 as dotStatus}
+				<div class="dot {dotStatus}" />
+			{/each}
+		</div>
+	</div>
+	<div class="flex">
+		<div class="digit" />
+		<div class="digit">{hours[0]}</div>
+		<div class="digit">{hours[1]}&nbsp;</div>
+		<div class="digit">{minutes[0]}</div>
+		<div class="digit">{minutes[1]}&nbsp;</div>
+		<div class="digit">{seconds[0]}</div>
+		<div class="digit">{seconds[1]}</div>
 	</div>
 </section>
 
-{#each Bmap as [key, value]}
-	<div>
-		<p>{key}:</p>
-		<p>{value}</p>
-	</div>
-{/each}
-
 <style>
-	div {
-		display: grid;
-		grid-template-columns: 3ch 1fr;
+	:root {
+		--size: 5rem;
 	}
-
 	.flex {
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		gap: 1rem;
+	}
+	.stack {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+	}
+	.digit,
+	.dot {
+		display: grid;
+		place-items: center;
+		width: var(--size);
+		height: var(--size);
+	}
+	.digit {
+		font-size: 4rem;
+	}
+	.dot {
+		border-radius: 50%;
+		background-color: rgba(255, 255, 255, 0.1);
+	}
+	.active {
+		background-color: rgba(255, 255, 255, 0.8);
 	}
 </style>
