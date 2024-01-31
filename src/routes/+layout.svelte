cd3bf6b08370e6a0af5fbde2817bf8d893f12850
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-01-31 12:28:05 -0800

    Fix typo in config.ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import '$lib/css/styles.css'
 	import { description, title } from '$lib/config.ts'
-	import { t } from 'vitest/dist/index-5aad25c1'
 </script>
 
 <svelte:head>
 	<meta property="og:locale" content="en_US" />
-	<meta property="og:title" content={t} />
+	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 </svelte:head>
 
